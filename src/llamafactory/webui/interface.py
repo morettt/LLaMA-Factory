b8687e9e81a823af98307ccca4718fa4ba1b4d6f
xxxiu <shuaigew94@gmail.com>
2026-06-14 17:39:33 +0800

    add dataset processing tab

--- src/llamafactory/webui/interface.py
+++ src/llamafactory/webui/interface.py
@@ -29,7 +29,7 @@ from .components import (
 )
 from .css import CSS
 from .engine import Engine
-from .extra_ui import create_distil_tab, create_extra_tab
+from .extra_ui import create_distil_tab, create_extra_tab, create_process_tab
 
 
 if is_gradio_available():
@@ -68,6 +68,9 @@ def create_ui(demo_mode: bool = False) -> "gr.Blocks":
 
         with gr.Tab("数据集蒸馏"):
             engine.manager.add_elems("distil", create_distil_tab())
+
+        with gr.Tab("数据集处理"):
+            engine.manager.add_elems("process", create_process_tab())
 
         engine.manager.add_elems("footer", create_footer())
         demo.load(engine.resume, outputs=engine.manager.get_elem_list(), concurrency_limit=None)
